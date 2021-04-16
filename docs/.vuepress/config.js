9f039e2cd09f72e0ad6facb652c413fdce9fc4eb
zhangbowen <zhangbowen.frontend@bytedance.com>
2021-04-17 00:52:26 +0800

    配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,8 +27,15 @@ module.exports = {
   ],
   themeConfig: {
     nav,
-    lastUpdated: '更新时间', // string | boolean
+    lastUpdated: '上次更新',
     logo: '/assets/img/logo.jpeg',
+    // 当你提供了 themeConfig.repo 选项，将会自动在每个页面的导航栏生成生成一个 GitHub 链接，以及在页面的底部生成一个 "Edit this page" 链接。
+    repo: 'topseczbw/single-bridge',
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    docsDir: 'docs',
+    editLinkText: '帮助zbw改善此页面',
+    docsBranch: 'main',
     // navbar: false,
     /** 根据文章目录自动生成侧边栏 */
     // sidebar: 'auto',
--- docs/about/about-child-ii/README.md
+++ docs/about/about-child-ii/README.md
@@ -683,8 +683,6 @@ partitionBy(numbers, n => n % 2 === 0); // [[1, 1, 3, 3], [4], [5, 5, 5]]
 partitionBy(numbers, n => n); // [[1, 1], [3, 3], [4], [5, 5, 5]]
 ```
 
-
-
 ## 移除
 
 ### 1. compact（falsy）
@@ -979,8 +977,6 @@ insertAt(myArray, 2, 5); // myArray = [1, 2, 3, 5, 4]
 let otherArray = [2, 10];
 insertAt(otherArray, 0, 4, 6, 8); // otherArray = [2, 4, 6, 8, 10]
 ```
-
-
 
 ## 改序
 
