049db894bb8512aeca8ec4702265f46b9509fdd7
张博文 <zhangbowen.frontend@bytedance.com>
2022-04-10 17:35:28 +0800

    update

--- docs/.vuepress/config/nav.config.js
+++ docs/.vuepress/config/nav.config.js
@@ -1,42 +1,44 @@
 module.exports = [
     { text: '首页', link: '/' },
     { text: '前言', link: '/guide/' },
-    { text: '系统', link: '/system/', items: [
-      { text: 'Linux', link: '/system/linux/' }
-    ] },
-    { text: '前端', items: [
-      { text: 'JavaScript', link: '/frontend/javascript/' },
-      { text: 'React', link: '/frontend/react/' },
-      { text: 'Vue', link: '/frontend/vue/' },
-      { text: 'CSS', link: '/frontend/css/' },
-      { text: 'TypeScript', link: '/frontend/typescript/' },
-      { text: 'Webpack', link: '/frontend/webpack/' },
-      { text: 'Utils & Lib', link: '/frontend/utils/' },
-      { text: '性能优化', link: '/frontend/performance/' },
-      { text: '数据结构与算法', link: '/frontend/data-structures-algorithms/' },
-      { text: '设计模式', link: '/frontend/design/' },
-      { text: '单元测试', link: '/frontend/jest/' },
-      { text: '技术方案', link: '/frontend/solution/' },
-      { text: '技术规范', link: '/frontend/standard/' },
-    ]},
-    { text: '后端', items: [
-      { text: 'Node', link: '/backend/node/' },
-      { text: 'Http', link: '/backend/http/' },
+    { text: '工作', items: [
+      { text: '前端', items: [
+        { text: 'javascript', link: '/frontend/javascript/' },
+        { text: 'react', link: '/frontend/react/' },
+        { text: 'vue', link: '/frontend/vue/' },
+        { text: 'webpack', link: '/frontend/webpack/' },
+        { text: 'css', link: '/frontend/css/' },
+        { text: 'typescript', link: '/frontend/typescript/' },
+        { text: 'util', link: '/frontend/utils/' },
+        { text: '性能优化', link: '/frontend/performance/' },
+        { text: '企业解决方案', link: '/frontend/solution/' },
+        { text: '团队规范', link: '/frontend/standard/' },
+        { text: '单元测试', link: '/frontend/jest/' },
       ]},
-    { text: '开发工具', items: [
-      { text: 'Git', link: '/tools/git/' },
-      { text: 'Github', link: '/tools/github/' },
-      { text: 'Gitlab', link: '/tools/gitlab/' },
-      { text: 'Webstorm', link: '/tools/webstorm/' },
-      { text: 'Chrome', link: '/tools/chrome/' },
-      { text: 'VSCode', link: '/tools/vscode/' },
-      { text: 'Charles', link: '/tools/charles/' },
-      { text: 'Mac', link: '/tools/mac/' },
+      { text: '后端', items: [
+        { text: 'node', link: '/backend/node/' },
+      ]},
+      { text: '通用', items: [
+        { text: 'http', link: '/backend/http/' },
+        { text: '数据结构与算法', link: '/frontend/data-structures-algorithms/' },
+        { text: '设计模式', link: '/frontend/design/' },
+      ]},
+      { text: '工具', items: [
+        { text: 'git', link: '/tools/git/' },
+        { text: 'github', link: '/tools/github/' },
+        { text: 'gitlab', link: '/tools/gitlab/' },
+        { text: 'webstorm', link: '/tools/webstorm/' },
+        { text: 'chrome', link: '/tools/chrome/' },
+        { text: 'vscode', link: '/tools/vscode/' },
+        { text: 'charles', link: '/tools/charles/' },
+        { text: 'mac', link: '/tools/mac/' },
+      ]},
+      { text: '其他', items: [
+        { text: '面试题', link: '/more/interview/' },
+        { text: '杂物室', link: '/more/hodgepodge/' },
+      ]},
     ]},
-    { text: '更多', items: [
-      { text: '读书笔记', link: '/more/book-note/' },
-      { text: '面试题', link: '/more/interview/' },
-      { text: '杂物室', link: '/more/hodgepodge/' },
+    { text: '生活', items: [
+      { text: '随笔', link: '/生活/随笔/' },
     ]},
-    { text: '关于我', link: '/about/'}
   ];
--- docs/README.md
+++ docs/README.md
@@ -3,17 +3,13 @@ home: true
 heroImage: cover.png
 heroText: zbw
 tagline: 唯格物者可以得物，唯不安者得以安宁
-actionText: Welcome!
+actionText: hi ! my bro
 actionLink: /guide/
 features:
 - title: 关于我
-  details: ☝️ 名某大厂程序员
+  details: 一名某大厂的前端
 - title: 爱好
   details: 音乐、读书、跑步
-- title: 联系方式
+- title: 联系我
   details: wx：13141270591
 ---
-
-::: tip
-一个写笔记的地方
-:::
